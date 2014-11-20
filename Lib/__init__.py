e7a272738538720f4c935a37fca0c5fc87475565
senya <nuaays@gmail.com>
2014-11-20 08:01:34 +0800

    Add pysftp.py for upload/download files

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -5,7 +5,7 @@
 
 import os, sys, platform, shutil
 sys.path.append( os.path.dirname( os.path.abspath(__file__) ) )
-print os.path.dirname( os.path.abspath(__file__) )
+sys.path.append( '..' )
 
 """
 """
